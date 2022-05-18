8df6472654b88c6940bf53894f5e47c776b9882a
ajaydaram <ajayforchrist777@gmail.com>
2022-05-18 18:14:28 +0530

    add about page and modified homepage

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Import react so you can use a react file
+import * as React from 'react'
+
+//define a component 
+
+const AboutPage = () => {
+    return(
+      <main>
+          <title>I am Ajay</title>
+          <h1>About Me</h1>
+          <p>This is the fucking 20th time i am trying</p>
+      </main>  
+    )
+}
+
+// export your component to use by other parts of the site
+ 
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,18 @@
+               //: Import React
+
 import * as React from 'react'
+
+      // Define your component
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to My Website!</h1>
+      <p1>This is 20th time i am trying!</p1>
+    </main>
+  )
+}
+
+                  // Export your component
+
+export default IndexPage
